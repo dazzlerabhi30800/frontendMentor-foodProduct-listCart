4bf4a986b5319e9800f0b380b53460f1098a16f6
dazzlerabhi30800 <abhisingh30800@gmail.com>
2025-02-18 05:11:49 +0530

    added redhat font

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ function populateProductWrapper() {
                 } 600w, ${image.desktop} 900w" alt=${name} />
                 <div class="product--controls">
 
-                 <div class='cart--controls bg-rose-600 ${
+                 <div class='cart--controls bg-rose-700 ${
                    searchItem ? "show" : "hide"
                  } '><button onclick="minusToCart('${name}')" class='minus--cart'><i class='fa-solid fa-minus'></i></button><span>${
                    searchItem ? searchItem.quantity : 0
